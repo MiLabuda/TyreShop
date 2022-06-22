7dc482bb92273766f46528b0baa1d3c4c3e7ad58
MiLabuda <mlabuda544@gmail.com>
2022-06-22 09:33:08 +0200

    refactor/refactoring templates

--- src/main/java/wszib/edu/pl/tyreshop/controllers/LoginController.java
+++ src/main/java/wszib/edu/pl/tyreshop/controllers/LoginController.java
@@ -1,19 +1,23 @@
 package wszib.edu.pl.tyreshop.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import wszib.edu.pl.tyreshop.model.User;
 import wszib.edu.pl.tyreshop.services.IUserService;
-import wszib.edu.pl.tyreshop.validator.UserValidator;
+import wszib.edu.pl.tyreshop.services.impl.UserServiceImpl;
+
+import javax.security.auth.login.LoginContext;
 
 @Controller
 public class LoginCotroller {
 
+    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
     private final IUserService userService;
 
     @Autowired
